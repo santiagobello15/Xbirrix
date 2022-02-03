c0d5b7364ea2b24ace5cdeb5dbe7849851bd3e43
santiagobello15 <santiagobello15@gmail.com>
2022-02-03 20:40:42 -0300

    finished the reviews react code. added media. will develop starts and score code

--- src/App.js
+++ src/App.js
@@ -12,27 +12,41 @@ function App() {
     }
   }
 
-  let reviews0 = new Reviews(0, "German", "No esta buena la app");
-  let reviews1 = new Reviews(1, "Esteban", "Muy buena la pagina");
+  let reviews0 = new Reviews(0, "German Gomez", "No esta buena la app");
+  let reviews1 = new Reviews(1, "Carolina Quito", "Muy buena la pagina");
+  let reviews2 = new Reviews(
+    2,
+    "Marta Morales",
+    "No tengo más que buenas palabras para la empresa. Llega todo siempre en buen estado, y rápido. Los precios también son buenos"
+  );
+  let reviews3 = new Reviews(
+    3,
+    "Jose Profeta",
+    "El sitio está muy bueno, pero los precios son elevados. Tienen gran variedad de bebidas."
+  );
 
   const [current, setCurrent] = useState(0);
 
-  let reviewsArray = [reviews0, reviews1];
+  let reviewsArray = [reviews0, reviews1, reviews2, reviews3];
   let reviewsFindId = reviewsArray.find((Reviews) => Reviews.userId == current);
-  console.log(reviewsFindId);
 
   const prevSlide = () => {
-    setCurrent(current - 1);
-    console.log(current);
+    if (current == 0) {
+      setCurrent(Object.keys(reviewsArray).length - 1);
+    } else {
+      setCurrent(current - 1);
+    }
   };
   const nextSlide = () => {
-    setCurrent(current + 1);
-    console.log(current);
+    if (current == Object.keys(reviewsArray).length - 1) {
+      setCurrent(0);
+    } else {
+      setCurrent(current + 1);
+    }
   };
 
   return (
     <div className="reviews-container1">
-      <div>{current}</div>
       <div className="reviews-picture picture-backgr"></div>
       <div className="reviews-picture">
         <img
