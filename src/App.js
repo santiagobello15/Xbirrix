64f5a162e119574757a1add5c62fa8e339bbc274
santiagobello15 <santiagobello15@gmail.com>
2022-02-02 23:37:34 -0300

    finally got to get the next and previous arrows to work. change name, picture and comments with react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 /* import FunReviews from "./ReviewsData.js"; intenté crear class y objects afuera, en otro archivo e importar pero no pude. no me tomaba los objects. PE reviews1*/
 
@@ -12,34 +12,50 @@ function App() {
     }
   }
 
-  let reviews1 = new Reviews(1, "German", "No esta buena la app");
-  let reviews2 = new Reviews(2, "Esteban", "Muy buena la pagina");
+  let reviews0 = new Reviews(0, "German", "No esta buena la app");
+  let reviews1 = new Reviews(1, "Esteban", "Muy buena la pagina");
+
+  const [current, setCurrent] = useState(0);
+
+  let reviewsArray = [reviews0, reviews1];
+  let reviewsFindId = reviewsArray.find((Reviews) => Reviews.userId == current);
+  console.log(reviewsFindId);
+
+  const prevSlide = () => {
+    setCurrent(current - 1);
+    console.log(current);
+  };
+  const nextSlide = () => {
+    setCurrent(current + 1);
+    console.log(current);
+  };
 
   return (
     <div className="reviews-container1">
+      <div>{current}</div>
       <div className="reviews-picture picture-backgr"></div>
       <div className="reviews-picture">
         <img
           className="reviews-image"
-          src={"/src/media/" + reviews1.userPicture + ".jpg"}
+          src={"/src/media/" + reviewsFindId.userPicture + ".jpg"}
         ></img>
       </div>
 
       <div className="arrow arrow-back-container">
-        <a href="a" className="arrow-a">
+        <a className="arrow-a" onClick={prevSlide}>
           {"<"}
         </a>
       </div>
       <div className="arrow arrow-forward-container">
-        <a href="a" className="arrow-a">
+        <a className="arrow-a" onClick={nextSlide}>
           {">"}
         </a>
       </div>
       <div className="reviews-title-container">
-        <a className="reviews-title-a">{reviews1.userName}</a>
+        <a className="reviews-title-a">{reviewsFindId.userName}</a>
       </div>
       <div className="reviews-body-container">
-        <a className="reviews-body-a">{reviews1.userComment}</a>{" "}
+        <a className="reviews-body-a">{reviewsFindId.userComment}</a>
       </div>
     </div>
   );
